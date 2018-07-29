a16dff80b70e09ce629b786e90622f8b2d5ac93a
Tamara <taamorim1@gmail.com>
2018-07-29 13:48:06 -0300

    Add project files.

---
+++ ExtraLongFactorials/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Numerics;
+
+namespace ExtraLongFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a numeric value ");
+            ExtraLongFactorials(EnterIntValue());
+            Console.ReadKey();
+        }
+
+        private static int EnterIntValue()
+        {
+            int c;
+            while(!int.TryParse(Console.ReadLine(), out c))
+            {
+                Console.WriteLine("Enter a numeric valid value");
+            }
+
+            return c;
+        }
+
+        static void ExtraLongFactorials(int n)
+        {
+
+            //long result = 1;
+            BigInteger result = new BigInteger(1);
+
+            for (int i = 0; i < n; i++)
+            {
+                result = result * (n - i);
+
+
+            }
+
+            Console.WriteLine("Factorial result: " + result);
+
+
+        }
+    }
+}
